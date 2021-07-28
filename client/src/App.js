46265bd58f4bfa2eb6711325de8dd325b8aa0db7
raimondas-27 <raimondasvilnius@yahoo.com>
2021-07-28 09:12:57 +0300

    react and node structure init, skeleton ready

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> hello from app </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
